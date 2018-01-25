3436b9b9f40c7f2ea140f5d991527873a9621b19
Y <yuming2046@126.com>
2018-01-25 20:56:40 +0800

    add

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const app = new Koa();
 
 app.use(router.routes());
 
+app.on('error', (error, ctx) => {
+    console.log('奇怪的错误' + JSON.stringify(ctx.onerror))
+    console.log('server error:' + error)
+})
+
+module.exports = app
+
 // app.listen(3000, () => {console.log('Koa start at 3000...')});
